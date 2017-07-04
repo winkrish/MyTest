35d3baccde3b222f8539382e44b9db98bceb4071
winkrish <bonagiri.krishna@gmail.com>
2017-07-03 22:17:18 -0500

    Base Version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("hi");
+                int i = 0;
+                int j = 0;
+                int k = i / j;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message+ ex.InnerException);
+            }
+        }
+    }
+}
